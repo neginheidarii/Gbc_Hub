dfe21faaef5dd064b028c4e8a2a2030ae9400a9a
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-10-26 13:54:27 -0400

    added dependencies, created index file for exporting all components/screens from a single file

---
+++ src/Components/index.js
@@ -0,0 +1,7 @@
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Card from './Card';
+import Hero from './Hero';
+import FooterBadge from './FooterBadge';
+
+export { Navbar, Footer, Card, Hero, FooterBadge };
