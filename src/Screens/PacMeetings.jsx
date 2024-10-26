38e51b1c85d9f012d4d14e9e15d6276f0dbbc27f
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-10-26 13:55:13 -0400

    implemented the structure of pac meetings  screen

---
+++ src/Screens/PacMeetings.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Navbar, Hero, FooterBadge, Footer } from "../Components";
+import { pac } from "../assets/index";
+
+function PacMeetings() {
+  return (
+    <div>
+      <Navbar />
+      <Hero
+        title="Shape Our Curriculum with Industry Expertise"
+        description={
+          "We’re committed to meeting the tech industry’s evolving needs. Join our Program Advisory Committee (PAC) to keep our curriculum cutting-edge. PAC meetings are held 1-2 times a year and usually online for your convenience. Your insights will help align our courses with industry trends and ensure our students are job-ready."
+        }
+        imgSrc={pac}
+        buttonText={"Join the PAC"}
+      />
+      <FooterBadge />
+      <Footer />
+    </div>
+  );
+}
+
+export default PacMeetings;
