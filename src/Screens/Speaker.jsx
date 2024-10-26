9f9b8cf6085274d5cbed24c66196c4d912f0ca2c
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-10-26 13:55:38 -0400

    implemented the structure of guest speakers screen

---
+++ src/Screens/Speaker.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Navbar, Footer, FooterBadge, Hero, Card } from "../Components";
+import { speaker } from "../assets/index";
+
+function Speaker() {
+  return (
+    <div>
+      <Navbar />
+      <Hero
+        title="Inspire the Next Generation of 
+Tech Leaders"
+        description="Share your expertise with our students and make a lasting impact. Whether you’re discussing industry trends, your career journey, or a specialized topic, your insights will inspire the future of tech. Choose the format that works best for you—seminars, workshops, online, or in person."
+        imgSrc={speaker}
+        buttonText={"Apply to Become a Guest Speaker"}
+      />
+
+      <FooterBadge />
+      <Footer />
+    </div>
+  );
+}
+
+export default Speaker;
