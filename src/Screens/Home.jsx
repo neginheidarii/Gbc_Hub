b630e659f2d089978890e870d4d324c294b6bf1c
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-10-26 13:55:03 -0400

    implemented the structure of home screen

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Hero, FooterBadge, Footer, Card, Navbar } from "../Components/index";
-import {pac, speaker, partner, hero, logo, project} from '../assets/index';
+import { pac, speaker, partner, hero, logo, project } from "../assets/index";
+
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <Hero />
+      <Hero
+        title="Connecting Industry with George Brown College"
+        description=" GBC Hub is the cornerstone of innovation and collaboration at George Brown College. We bring together industry professionals, educators, and students to create a dynamic learning environment where real-world challenges meet cutting-edge education. Join us in shaping the future of tech education and driving impactful change across the industry."
+        imgSrc={hero}
+        buttonText="Learn more"
+      />
       <div className="bg-[#005AA5] text-[#ffff] text-center w-full h-72">
         <p className="font-inter text-4xl font-semibold leading-[61.5px] tracking-tight text-center py-9 ">
           Why GBC Hub?
@@ -19,7 +25,7 @@ const Home = () => {
           of tech education and driving impactful change across the industry.
         </p>
       </div>
-      <div className="bg-blue-200 flex flex-col text-center py-10">
+      <div className="bg-[#e7f6ff] flex flex-col text-center py-10">
         <h1 className="font-inter text-4xl font-semibold tracking-tight text-center py-10">
           Explore Collaboration Opportunities
         </h1>
@@ -30,8 +36,18 @@ const Home = () => {
             description="Share your projects and help students gain real-world experience while benefiting from fresh ideas."
             buttonText="Learn more"
           />
-          <Card />
-          <Card />
+          <Card
+            imgSrc={pac}
+            title="PAC Meetings for Course Evaluations"
+            description="Join our committee and help align our courses with industry trends."
+            buttonText="Learn more"
+          />
+          <Card
+            imgSrc={speaker}
+            title="Guest Speaker Invitation"
+            description="Share your expertise with the next generation of tech professionals."
+            buttonText="Learn more"
+          />
         </div>
       </div>
       <div className="">
