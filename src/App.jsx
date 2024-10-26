017c355dd74cc0c79e70add8c35a79ed842c2f6a
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-10-26 13:57:40 -0400

    used react router for linking the paths to the right elemnt

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./Screens/Home";
+import { Home, ProjectIdeas, PacMeetings, Speaker, Contact } from "./Screens";
+
 const App = () => {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project-ideas" element={<ProjectIdeas />} />
+        <Route path="/pac-meetings" element={<PacMeetings />} />
+        <Route path="/guest-speaker" element={<Speaker />} />
+        <Route path="/contact-us" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 };
 
