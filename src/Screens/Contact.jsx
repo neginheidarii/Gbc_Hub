5aa1c21c6be597cbe38c2576fbced76b672932ea
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-10-27 20:29:03 -0400

    Added form for the contact screen

--- src/Screens/Contact.jsx
+++ src/Screens/Contact.jsx
@@ -1,19 +1,35 @@
-import React from 'react'
+import React from "react";
 import { Navbar, Footer, FooterBadge, Hero, Card } from "../Components";
 
 function Contact() {
   return (
     <div>
       <Navbar />
-      <Hero
-        title="Let’s Connect!"
-        description="Got questions about these opportunities? Reach out at coordinator@gbchub.ca or fill out the form  to send us a message. We're here to help!"
-      />
+      <div className="flex justify-center items-center bg-[#e7f6ff]">
+        <div className="w-2/3">
+          <Hero
+            title="Let’s Connect!"
+            className="w-1/2"
+            description="Got questions about these opportunities? Reach out at coordinator@gbchub.ca or fill out the form  to send us a message. We're here to help!"
+          />
+        </div>
+        <div className="w-1/3 mr-10">
+          <Card
+            formInput={{
+              name: "Name",
+              type: "text",
+              placeholder: "Enter your name",
+              className: "", 
+            }}
+            className=""
+          />
+        </div>
+      </div>
 
       <FooterBadge />
-        <Footer />
+      <Footer />
     </div>
   );
 }
 
-export default Contact
+export default Contact;
