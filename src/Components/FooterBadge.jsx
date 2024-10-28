9dbaf8bf6de0c445685599d46c4e8d172abb78c7
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-10-27 20:53:50 -0400

    Impoved the style of badge

--- src/Components/FooterBadge.jsx
+++ src/Components/FooterBadge.jsx
@@ -3,32 +3,36 @@ import pic from "../assets/images/partner.jpg";
 
 const FooterBadge = () => {
   return (
-    <>
-      <div className="relative h-64 rounded-md shadow-xl flex p-8 bg-[#e7f6ff]">
-        <img
-          src={pic}
-          alt="Shoes"
-          className=" w-full object-cover rounded-xl inset-0 bg-black opacity-30 border-2 border-black"
-        />
-        <div className="absolute inset-0 flex justify-center items-center">
-          <div className="relative z-10 text-left space-y-4">
-            <h2 className="text-black font-extrabold text-3xl">
-              Become a Partner Today
-            </h2>
-            <p className="text-black">
-              Join us in shaping the future of tech education. Partner with us
-              to make a lasting impact on both students and your industry.
-            </p>
-            <div className="flex justify-start">
-              <a className="btn bg-[#FECB00] w-32 font-inter font-medium text-center">
-                Join Now
-              </a>
-            </div>
-          </div>
+    <div className="relative h-64 rounded-md shadow-sm flex p-8 bg-[#e7f6ff]">
+      {/* Image with gradient overlay */}
+      <div className="relative w-full h-full rounded-xl overflow-hidden">
+        <img src={pic} alt="Shoes" className="w-full h-full object-cover" />
+        <div className="absolute inset-0 bg-gradient-to-r from-[#005AA5] to-[#00A1DE] opacity-70"></div>
+      </div>
+
+      {/* Content overlay */}
+      <div className="absolute inset-0 flex items-center justify-between p-8">
+        {/* Left Text Section */}
+        <div className="space-y-4 text-left ml-14">
+          <h2 className="text-white font-extrabold text-3xl">
+            Become a Partner Today
+          </h2>
+          <p className="text-white">
+            Join us in shaping the future of tech education. Partner with us to
+            make a lasting impact on both students and your industry.
+          </p>
+        </div>
+
+        {/* Right Button Section */}
+        <div>
+          <a className="btn bg-[#FECB00] w-32 font-inter font-medium text-center mr-10">
+            Join Now
+          </a>
         </div>
       </div>
-    </>
+    </div>
   );
 };
+
 
 export default FooterBadge;
