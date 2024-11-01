76ac3c6a8bdbf0719a06fc57218e732d85c92ca1
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-11-01 16:02:08 -0400

    minor text changes

--- src/Screens/Speaker.jsx
+++ src/Screens/Speaker.jsx
@@ -11,7 +11,7 @@ function Speaker() {
 Tech Leaders"
         description="Share your expertise with our students and make a lasting impact. Whether you’re discussing industry trends, your career journey, or a specialized topic, your insights will inspire the future of tech. Choose the format that works best for you—seminars, workshops, online, or in person."
         imgSrc={speaker}
-        buttonText={"Apply to Become a Guest Speaker"}
+        buttonText={"Become a Guest Speaker"}
       />
 
       <FooterBadge />
