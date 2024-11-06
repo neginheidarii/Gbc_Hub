4aeedf5d7be983099ff3deaffe98b518cee51dab
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-11-06 13:53:19 -0500

    added navigation

--- src/Screens/Speaker.jsx
+++ src/Screens/Speaker.jsx
@@ -12,6 +12,7 @@ Tech Leaders"
         description="Share your expertise with our students and make a lasting impact. Whether you’re discussing industry trends, your career journey, or a specialized topic, your insights will inspire the future of tech. Choose the format that works best for you—seminars, workshops, online, or in person."
         imgSrc={speaker}
         buttonText={"Become a Guest Speaker"}
+        NavButton={"/speakers-form"}
       />
 
       <FooterBadge />
