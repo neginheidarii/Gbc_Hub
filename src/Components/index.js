12c113688cb0fbbd2dc8c9e708120fb41673690b
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-11-07 09:04:35 -0500

    updated components import

--- src/Components/index.js
+++ src/Components/index.js
@@ -3,5 +3,6 @@ import Footer from './Footer';
 import Card from './Card';
 import Hero from './Hero';
 import FooterBadge from './FooterBadge';
+import Programs from './Programs';
 
-export { Navbar, Footer, Card, Hero, FooterBadge };
+export { Navbar, Footer, Card, Hero, FooterBadge, Programs };
