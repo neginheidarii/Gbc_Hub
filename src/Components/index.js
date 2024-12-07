73fc1a8235eff906987b11fc1719ccc6ee06ed49
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-12-07 16:49:22 -0500

    added backend folder

--- src/Components/index.js
+++
@@ -1,8 +0,0 @@
-import Navbar from './Navbar';
-import Footer from './Footer';
-import Card from './Card';
-import Hero from './Hero';
-import FooterBadge from './FooterBadge';
-import Programs from './Programs';
-
-export { Navbar, Footer, Card, Hero, FooterBadge, Programs };
--- src/assets/index.js
+++
@@ -1,8 +0,0 @@
-import speaker from "/images/speaker.jpg";
-import logo from "/logo/logo.png";
-import hero from "/images/hero.png";
-import partner from "/images/partner.jpg";
-import pac from "/images/pac.jpg";
-import project from "/images/project.jpg";
-
-export { speaker, logo, hero, partner, pac, project };
